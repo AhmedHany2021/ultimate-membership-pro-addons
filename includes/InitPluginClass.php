7db4cfde2e51404702d773cdbcd543035b18e97f
AhmedHany2021 <cehu90@gmail.com>
2025-01-28 04:17:12 +0200

    cahnge version

--- includes/InitPluginClass.php
+++ includes/InitPluginClass.php
@@ -6,12 +6,12 @@ class InitPluginClass
 {
     public function __construct()
     {
-        add_action( 'init',[$this , 'init']); 
+        add_shortcode("test", [$this, "test"]);
     }
 
-    public function init()
+    public function test()
     {
-        remove_action( 'wp_ajax_ihc_register_forms_check_one_field', [ '\Indeed\Ihc\RegisterForm', 'ihc_register_forms_check_one_field' ] );
-        remove_action( 'wp_ajax_nopriv_ihc_register_forms_check_one_field', [ '\Indeed\Ihc\RegisterForm', 'ihc_register_forms_check_one_field' ] );
+        echo "test";
     }
+
 }
